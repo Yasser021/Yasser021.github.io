6683b87ff9bc29ba1452d56c0066275b358c5c09
Yasser <azisalfalahyasser@gmail.com>
2024-06-16 00:15:25 +0700

    add: add About section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Navbar from "./navbar";
 import Hero from "./hero";
+import About from "./about";
+import Projek from "./projek";
 
 function App() {
   return (
@@ -8,21 +10,11 @@ function App() {
       <header className="">
         <Navbar />
       </header>
-      <main className="container mx-auto mt-10">
-        <Hero/>
-        {/* <section id="about" className="mt-10">
-          <h1 className="text-3xl font-bold">About</h1>
-          <p>Learn more about us.</p>
-        </section>
-        <section id="services" className="mt-10">
-          <h1 className="text-3xl font-bold">Services</h1>
-          <p>Discover our services.</p>
-        </section>
-        <section id="contact" className="mt-10">
-          <h1 className="text-3xl font-bold">Contact</h1>
-          <p>Get in touch with us.</p>
-        </section> */}
+      <main className="container mx-auto mt-10 py-10">
+        <Hero />
       </main>
+      <About />
+      <Projek/>
     </div>
   );
 }
