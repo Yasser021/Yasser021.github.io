12295b91cb64fc6773884d12db45f5fc8434d890
Yasser <azisalfalahyasser@gmail.com>
2024-06-21 21:31:17 +0700

    chore: adjusting padding

--- src/about.js
+++ src/about.js
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <div className="bg-black text-white py-10">
-      <section id="about" className="py-5 container mx-auto my-5">
+      <section id="about" className="py-5 container mx-auto my-5 p-8">
         <h1 className="text-3xl font-semibold">About Me</h1>
         <p className="mt-3 text-justify">
           My name is <b>YASSER AZIZ ALFALAH</b>, a fresh graduate from <b>SMKN 4 TANGERANG
